e47e8e47f66cf990e7eae5acc9b33cd18ccfaba5
Logeshwaranr0902 <96953357+Logeshwaranr0902@users.noreply.github.com>
2024-09-22 20:29:24 +0530

    Update App.js

--- emotion_analyzer/src/App.js
+++ emotion_analyzer/src/App.js
@@ -26,7 +26,7 @@ function App({ nightMode }) {
 
     setLoading(true);
     try {
-      const response = await axios.post('https://backendfile-emotion-analyzer.onrender.com', formData, {
+      const response = await axios.post('https://backendfile-emotion-analyzer.onrender.com/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
